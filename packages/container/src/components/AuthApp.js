e7a733b2e47c3b0d8f5e7ad9310f252ae608368f
G-PPeralta <prates_peralta@hotmail.com>
2023-03-24 10:49:09 -0300

    fix: clean cache

--- packages/container/src/components/AuthApp.js
+++ packages/container/src/components/AuthApp.js
@@ -1,6 +1,6 @@
-import { mount } from "auth/AuthApp";
-import React, { useRef, useEffect } from "react";
-import { useHistory } from "react-router-dom";
+import { mount } from 'auth/AuthApp';
+import React, { useRef, useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
 
 export default () => {
   const ref = useRef(null);
@@ -9,13 +9,13 @@ export default () => {
   useEffect(() => {
     const { onParentNavigate } = mount(ref.current, {
       initialPath: history.location.pathname,
-      onNavigate: ({ pathname: nextPathName }) => {
+      onNavigate: ({ pathname: nextPathname }) => {
         const { pathname } = history.location;
 
-        if (pathname !== nextPathName) {
-          history.push(nextPathName);
+        if (pathname !== nextPathname) {
+          history.push(nextPathname);
         }
-      }
+      },
     });
 
     history.listen(onParentNavigate);
